e49eb7e6e55edd02542ae122198b19b310870ab1
Christian Tanicala <christian.tanicala@gmail.com>
2021-10-31 20:09:43 -0400

    created models

---
+++ models/review.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Review.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    review_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    review_description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'review',
+  }
+);
+
+module.exports = Review;
